fa253cdce34c3d1af915a4c2e3dcd200db33f60c
fandong@email.arizona.edu <fandong@email.arizona.edu>
2025-02-25 02:33:08 -0600

    deploy

---
+++ client/build/precache-manifest.12f9e28c01b256a90478a97fd486c3c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee4495bfaff4207c0cfa",
+    "url": "/static/css/main.d2b0a0d0.chunk.css"
+  },
+  {
+    "revision": "ee4495bfaff4207c0cfa",
+    "url": "/static/js/main.147f995c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c96fc6641f89712a107f",
+    "url": "/static/js/2.b9a961a7.chunk.js"
+  },
+  {
+    "revision": "5cadeda238e77bd96ea3fbd034fff867",
+    "url": "/static/media/home.5cadeda2.png"
+  },
+  {
+    "revision": "340b13470ddcea2c8ce46ce48963f6ad",
+    "url": "/static/media/model.340b1347.png"
+  },
+  {
+    "revision": "e9e646face9e476f021699510c05bf74",
+    "url": "/index.html"
+  }
+];
