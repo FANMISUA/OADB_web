11d745bab4f60091ddacb2c06cebd86b25746174
fandong@email.arizona.edu <fandong@email.arizona.edu>
2025-02-25 04:43:33 -0600

    add

--- client/src/component/search/structuresearch.js
+++ client/src/component/search/structuresearch.js
@@ -1,77 +1,78 @@
 import { Button, Modal } from 'antd'
 import React, { useState } from 'react'
+const port = process.env.REACT_APP_PORT || 6000
 
 function StructureSearch () {
 
-const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-const [visible,setVisible] = useState(false)
+    const [visible, setVisible] = useState(false)
 
-const showModal = () => {
-    setVisible(true)
-}
+    const showModal = () => {
+        setVisible(true)
+    }
 
-const searchKetcher = async(value) => {
-        const response = await fetch(`http://localhost:5000/demosdf/${value}`)
+    const searchKetcher = async (value) => {
+        const response = await fetch(`http://localhost:${port}/demosdf/${value}`)
         const jsonData = await response.json()
         console.log('search structure...')
         console.log(jsonData)
-       
-}
 
-
-
-const loadKetcher = async() => {
-    var ketcherFrame = document.getElementById('Ketcher2.5')
-    var ketcher = null
-    ketcher = ketcherFrame.contentWindow.ketcher
-    var smile = await ketcher.getSmiles()
-    console.log(smile)
-    searchKetcher(smile)
     }
 
-const handleOk = () => {
-setLoading(true)
-loadKetcher()
-setTimeout(() => {
-setLoading(false)
-setVisible(false)
-}, 3000)
-}
 
-const handleCancel = () => {
-setVisible(false)
-}
 
-return (
-<>
-<Button type="link" block onClick={showModal}>
-Structure Search
-</Button>
+    const loadKetcher = async () => {
+        var ketcherFrame = document.getElementById('Ketcher2.5')
+        var ketcher = null
+        ketcher = ketcherFrame.contentWindow.ketcher
+        var smile = await ketcher.getSmiles()
+        console.log(smile)
+        searchKetcher(smile)
+    }
 
-<Modal
-visible={visible}
-title="Structure Search"
-onOk={handleOk}
-onCancel={handleCancel}
-width={900}
-footer={[
-<Button key="back" onClick={handleCancel}>
-Return
-</Button>,
+    const handleOk = () => {
+        setLoading(true)
+        loadKetcher()
+        setTimeout(() => {
+            setLoading(false)
+            setVisible(false)
+        }, 3000)
+    }
 
-<Button key="Search" type="primary" loading={loading} onClick={handleOk}>
-Search
-</Button>,
-]}
->
-<div> 
-<iframe id="Ketcher2.5" title="ketcher2.5" src="ketcher/index.html" width="800" height="600"></iframe>
+    const handleCancel = () => {
+        setVisible(false)
+    }
 
-</div>
-</Modal>
-</>
-)
+    return (
+        <>
+            <Button type="link" block onClick={showModal}>
+                Structure Search
+            </Button>
+
+            <Modal
+                open={visible}
+                title="Structure Search"
+                onOk={handleOk}
+                onCancel={handleCancel}
+                width={900}
+                footer={[
+                    <Button key="back" onClick={handleCancel}>
+                        Return
+                    </Button>,
+
+                    <Button key="Search" type="primary" loading={loading} onClick={handleOk}>
+                        Search
+                    </Button>,
+                ]}
+            >
+                <div>
+                    <iframe id="Ketcher2.5" title="ketcher2.5" src="ketcher/index1.html" width="800" height="600"></iframe>
+
+                </div>
+            </Modal>
+        </>
+    )
 }
 
 
